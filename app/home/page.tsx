28baa1a608ade34ce9800cf0abf542d1b295db09
Daniel-627 <ochiengdaniel627@gmail.com>
2024-11-12 22:26:39 +0300

    Homepage trial

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import PopularBlogCard from "@/components/PopularBlogCard";
+import PopularBlogCards from "@/components/PopularBlogCards";
 
 type Props = {}
 
 export default function page({}: Props) {
   return (
-    <div>page</div>
+    <div>
+      <PopularBlogCard/>
+      
+      <PopularBlogCards/>
+    </div>
   )
 }
