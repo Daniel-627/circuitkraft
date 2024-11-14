cc7528ce1729e700029b52734a5f8a0cbce0580b
Daniel-627 <ochiengdaniel627@gmail.com>
2024-11-14 22:51:25 +0300

    Styling home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import PopularBlogCard from "@/components/PopularBlogCard";
-import PopularBlogCards from "@/components/PopularBlogCards";
+
+import MainPage from '@/components/MainPage';
+import AllWidgets from '@/components/AllWidgets';
 
 type Props = {}
 
 export default function page({}: Props) {
   return (
     <div>
-      <PopularBlogCard/>
-      
-      <PopularBlogCards/>
+      <div className='grid grid-cols-12'>
+        <div className='col-span-9'>
+          <MainPage />
+        </div>
+        <div className='col-span-3'>
+          <AllWidgets />
+        </div>
+      </div>
     </div>
   )
 }
